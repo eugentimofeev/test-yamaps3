404201a2610a329f8a0c76d54c66ba2af4e30f87
eugentimofeev <56805062+eugentimofeev@users.noreply.github.com>
2022-12-09 15:28:03 +0300

    del some files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr(), basicSsl()],
-	server: {
-		host: "www.baidu--123.cn",
-		port: 443,
-		https: true,
-	},	
+  plugins: [react(), svgr()],
 });
+
+/*
+  localhost
+  import basicSsl from '@vitejs/plugin-basic-ssl';
+  export default defineConfig({
+    plugins: [react(), svgr(), basicSsl()],
+    server: {
+      host: "www.baidu--123.cn",
+      port: 443,
+      https: true,
+    },	
+  });
+*/
